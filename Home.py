0fe780379bb11789912f147dc3be247a8d1b3b54
Britojuliano99 <britojuliano99@gmail.com>
2022-07-18 17:41:58 -0300

    iae

--- Home.py
+++ Home.py
@@ -15,11 +15,19 @@ st.number_input("Your height?(m)",key="height")
 
 
 if st.button("Your IMC is:"):
+    
     Peso=st.session_state.weight
     Altura=st.session_state.height
-    Imc=Peso/Altura**2
+    try:
+        Imc=Peso/Altura**2
     
-    st.write(f"{st.session_state.name} your current IMC is {Imc}")
-
+        st.write(f"{st.session_state.name} your current IMC is {Imc}")
+        if Imc>30:
+            st.write("É bom ir para a academia viu...")
+    except:
+        st.write("Você não disse seu peso nem sua altura ai fica dificil")
 if st.button("Mensagem do dia"):
-    st.markdown(f"Você {st.session.name} é lindo(a)")
+    try:
+        st.markdown(f"Você {st.session.name} é lindo(a)")
+    except:
+        st.markdown("Qual é seu nome?")
