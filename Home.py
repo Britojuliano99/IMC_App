fe0659eb14fa28b8b22fa8930c867dfb31bb6032
Britojuliano99 <98929013+Britojuliano99@users.noreply.github.com>
2022-07-18 17:53:13 -0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,33 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.write("IMC calculator")
+
+st.text_input("Your name", key="name")
+st.number_input("Your age", key="age")
+st.number_input("Your current weigth ?",key="weight")
+st.number_input("Your height?(m)",key="height")
+
+
+if st.button("Your IMC is:"):
+    
+    Peso=st.session_state.weight
+    Altura=st.session_state.height
+    try:
+        Imc=Peso/Altura**2
+    
+        st.write(f"{st.session_state.name} your current IMC is {Imc}")
+        if Imc>30:
+            st.write("É bom ir para a academia viu...")
+    except:
+        st.write("Você não disse seu peso nem sua altura ai fica dificil")
+if st.button("Mensagem do dia"):
+    try:
+        st.markdown(f"Você {st.session.name} é lindo(a)")
+    except:
+        st.markdown("Qual é seu nome?")
